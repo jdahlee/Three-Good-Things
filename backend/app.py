d08eb66b105d056bff298a9e9e6a07b0d5a408a2
jdahlee <judahlee29@gmail.com>
2025-02-18 02:55:24 +0000

    Import and add Cors and requests packages to backend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World This is a test! The backend is working</p>"
+    return "<p>Hello World! The backend is working</p>"
