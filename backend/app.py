71c004ccb817e7ee4a20a1e332b1bf15e83ab163
ethan-nguyen8 <ethnguyen38@gmail.com>
2025-03-28 17:36:23 -0500

    init firebase

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 import requests
+import firebase_admin
+from firebase_admin import firestore
+
+app = firebase_admin.initialize_app()
+db = firestore.client()
 
 app = Flask(__name__)
 CORS(app)
